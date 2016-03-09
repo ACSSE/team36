bc919f196fc5b26245739ca5a9eb0dce59d8bc81
Richard Faul (215022652) <215022652@student.uj.ac.za>
2016-03-09 02:11:59 +0200

    Add sql script processing functionality - Update comments - Add runCommands() method - Add executeSQLScriptFile() method - Add multiple command report functionality - Add more getters and setters - Add more helper functions - Fix general bugs and logic flaws - Remove old test cases - Update TODO

--- TestCases.php
+++ TestCases.php
@@ -16,41 +16,18 @@
 </head>
 <body>
 <?php
+    //Improves performance of text output functions
+    ob_start();
+    //Output
     echo "<pre>";
-
-    //TEST CASES FOR DatabaseModule.php
-    $dbHandler = new ServerSessionDatabaseHandler("localhost","root","Sebenza","SebenzaSA_Database");
-    if ($dbHandler->runCommand("SELECT * FROM `REGISTERED_USER`")) {
-        echo $dbHandler->getResultsInJSON()."\n";
+    $startTime = microtime(true);
+    $dbHandler = new ServerSessionDatabaseHandler("localhost","root","Sebenza");
+    if($dbHandler->executeSQLScriptFile('./database/SebenzaSA_Database.sql')) {
+        print_r($dbHandler->getCommandsReport());
+    } else {
+        print_r($dbHandler->getErrors());
     }
-
-    //Just for the sake of initiating the variable
-    $insertID = 10;
-
-    if ($dbHandler->runCommand("INSERT INTO `REGISTERED_USER` (`Username`, `Email`, `ContactNumber`, `TypeOfUser`, `Password`) VALUES (?,?,?,?,?)",'phpInsertedUser', 'userphp@email.co.za', '0829494321', 3, 'unhashedPassword3')) {
-        $insertID = $dbHandler->getInsertID();
-        echo "Insert ID: ".$insertID."\n";
-        echo "Number of rows affected: ".$dbHandler->getAffectedRowCount()."\n";
-    }
-
-    if ($dbHandler->runCommand("SELECT * FROM `REGISTERED_USER`")) {
-        print_r($dbHandler->getResults());
-    }
-
-    if($dbHandler->runCommand("UPDATE `REGISTERED_USER` SET `Username` = ? WHERE `UserID` = ?",'Alice in Wonderland',$insertID)) {
-        echo "Number of rows affected: ".$dbHandler->getAffectedRowCount()."\n";
-    }
-
-    if ($dbHandler->runCommand("SELECT * FROM `REGISTERED_USER`")) {
-        echo $dbHandler->getResults()[2]["Username"]."\n";
-    }
-
-    $dbHandler->runCommand("DELETE FROM `REGISTERED_USER` WHERE `UserID` = ?", $insertID);
-
-    if ($dbHandler->runCommand("SELECT * FROM `REGISTERED_USER`")) {
-        print_r($dbHandler->getResults());
-    }
-
+    echo (microtime(true) - $startTime)." seconds runtime.";
     echo "</pre>";
 ?>
 
