420a23298984c5ee075187a1131b5ff9296ccd89
Richard Faul (215022652) <215022652@student.uj.ac.za>
2016-03-28 14:39:54 +0200

    Add minimal top bar functionality - login modal added - required foundation files added and links fixed

--- php/interface/topbar.php
+++ php/interface/topbar.php
@@ -20,43 +20,114 @@
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
 </head>
 <body>
-<!--DOCUMENT START-->
-<div class="row collapse logoHeight fullWidth">
-    <div class="shrink columns logoWidth fullHeight backgroundColor">
-        <img class="logoHeight backgroundColor" type="image/svg+xml" src="../../images/logo.svg" alt="logo"/>
-    </div>
-    <div class="small-expand medium-expand large-expand columns fullHeight backgroundColor">
-        <div class="row collapse navBarBackgroundHeight fullWidth">
-            <div class="columns">
+    <div class="page-container">
+    <!--DOCUMENT START-->
+    <header class="sebenza-top-bar">
+        <div class="row collapse logo-height full-width">
+            <div class="shrink columns logo-width full-height background-colour">
+                <img class="logo-height background-colour" type="image/svg+xml" src="../../images/logo.svg" alt="logo"/>
+            </div>
+            <div class="small-expand medium-expand large-expand columns full-height background-colour">
+                <div class="row collapse navBarBackgroundHeight full-width">
+                    <!--Place holder row for background colour-->
+                </div>
+                <div class="row collapse navigation-bar-colour full-width navBarHeight">
+                    <div class="expanded columns full-height">
+                        <span class="navigation-bar">
+                        <!--http://foundation.zurb.com/sites/docs/button-group.html-->
+                        <!--http://foundation.zurb.com/sites/docs/button.html-->
+                        <ul class="menu align-right hide-for-small-only">
+                            <li>
+                                <button type="button" class="warning button" >
+                                    Area Information
+                                    <img class="top-bar-button-icon" type="image/svg+xml" src="../../images/info-icon.svg" alt="logo"/>
+                                </button>
+                            </li>
+                            <li>
+                                <button type="button" class="button" data-toggle="login-medium-large">
+                                    Log In
+                                    <img class="top-bar-button-icon" type="image/svg+xml" src="../../images/user-icon.svg" alt="logo"/>
+                                </button>
+                            </li>
+                        </ul>
+                        <ul class="menu align-right show-for-small-only">
+                            <li>
+                                <button type="button" class="button">
+                                    Menu
+                                    <img class="top-bar-button-icon" type="image/svg+xml" src="../../images/menu-icon.svg" alt="logo"/>
+                                </button>
+                            </li>
+                        </ul>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row collapse navBarColour fullWidth navBarHeight">
-            <div class="small-expand medium-expand large-expand columns fullHeight">
-                <!--<div class="top-bar-title">
-                    <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
-                        <span class="menu-icon" data-toggle></span>
-                    </span>
+    </header>
+        <!--Login modal for medium and large screens-->
+        <!--http://foundation.zurb.com/sites/docs/toggler.html-->
+        <div class="background-colour hide-for-small-only" id="login-medium-large" data-toggler data-animate="hinge-in-from-top hinge-out-from-top">
+            <!--http://foundation.zurb.com/sites/docs/forms.html-->
+            <form>
+                <div class="row">
+                    <div class="columns">
+                        <label><b>Username</b>
+                            <input type="text" class="smaller-input-box" placeholder="Username">
+                        </label>
+                    </div>
                 </div>
-                <div id="responsive-menu" class="fullHeight">
-                <div class="top-bar-right navBarColour fullHeight navBarRightPadding">
-                    <ul class="menu navBarColour fullHeight" >
-                        <li><a href="#" class="button navBarButton">LOGIN</a></li>
-                        <li><a href="#" class="button navBarButton">SIGN UP</a></li>
-                    </ul>
+                <div class="row">
+                    <div class="columns">
+                        <label><b>Password</b>
+                            <input type="text" class="smaller-input-box" placeholder="Password">
+                        </label>
+                    </div>
                 </div>
-                </div>-->
+                <div class="row top-padding">
+                    <div class="columns">
+                        <button type="button" class="success button full-width" >
+                            Log In
+                        </button>
+                    </div>
+                </div>
+                <div class="row top-padding">
+                    <div class="columns">
+                        <button type="button" class="button full-width" >
+                            Register
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    <div class="content-view">
+
+    </div>
+    <footer class="sebenza-bottom-bar navigation-bar-colour">
+        <div class="row site-links small-up-2 medium-up-4 large-up-4">
+            <div class="columns center-contents">
+                <a href="#1" class="yellow-link">Contact Us</a>
+            </div>
+            <div class="columns center-contents">
+                <a href="#2" class="yellow-link">Privacy Policy</a>
+            </div>
+            <div class="columns center-contents">
+                <a href="#3" class="yellow-link">Terms and Conditions</a>
+            </div>
+            <div class="columns center-contents">
+                <a href="#4" class="blue-link">About Us</a>
             </div>
         </div>
+    </footer>
+    <!--DOCUMENT END-->
     </div>
-</div>
-<p>ubuntu</p>
-<!--DOCUMENT END-->
-<!--Required by Foundation 6-->
-<script src="../../foundation-6/js/vendor/jquery.min.js"></script>
-<script src="../../foundation-6/js/foundation.js"></script>
-<!-- Other JS plugins can be included here -->
-<script>
-    $(document).foundation();
-</script>
+    <!-- Foundation required code-->
+    <script src="../../foundation-6/js/jquery.js"></script>
+    <script src="../../foundation-6/js/what-input.js"></script>
+    <script src="../../foundation-6/js/foundation.js"></script>
+    <script src="../../foundation-6/js/app.js"></script>
+    <!-- Other JS plugins can be included here -->
+    <script>
+        $(document).foundation();
+    </script>
 </body>
 </html>
