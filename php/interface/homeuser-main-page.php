c5c6b57916d77ec58142654bd354cf03b89d0269
Nicholas Rader <201325732@student.uj.ac.za>
2016-05-20 09:01:30 +0200

    #	contactpage.php

--- php/interface/homeuser-main-page.php
+++ php/interface/homeuser-main-page.php
@@ -24,12 +24,20 @@
             <div class="tabs-panel full-height is-active" id="panel1v">
                 <h1>Request Trade Workers</h1>
                 <div class="row">
-                    <div class="large-12 medium-12 columns">
-                        <input id="checkbox1" type="checkbox"><label for="checkbox1">Painter</label>
-                        <input id="checkbox2" type="checkbox"><label for="checkbox2">Tiler</label>
-                        <input id="checkbox3" type="checkbox"><label for="checkbox3">Paver</label>
-                        <input id="checkbox4" type="checkbox"><label for="checkbox4">Tree-Feller</label>
+                    <div class="large-6 small-12 columns">
+                        <label>Sort By:</label>
+                        <select>
+                            <option value="option1">Painter</option>
+                            <option value="option2">Tiler</option>
+                            <option value="option3">Paver</option>
+                            <option value="option4">Tree-Feller</option>
+
+                        </select>
                     </div>
+
+                </div>
+                <div class="row">
+
                     <div class="large-12 medium-12 columns">
                         <img class="thumbnail" src="Images/tempUserImage.png" alt="Photo of User." style="width: 100px;height: 100px;">
                         <input type="radio" name="worker" value="worker1" id="worker1"><label for="worker1">Joe Mason</label>
@@ -44,7 +52,7 @@
                 <hr>
                 <div class="row">
                     <div class="large-12 medium-12 columns">
-                        <a href="#" class="float-left medium secondary button radius">Notify TradeWorker</a>
+                        <a href="#" class="float-left medium secondary button radius">Request TradeWorker</a>
                     </div>
                 </div>
             </div>
@@ -71,7 +79,7 @@
                 <hr>
                 <div class="row">
                     <div class="large-12 medium-12 columns">
-                        <a href="#" class="float-left medium secondary button radius">Notify Contractor</a>
+                        <a href="#" class="float-left medium secondary button radius">Request Contractor</a>
                     </div>
                 </div>
             </div>
@@ -97,6 +105,21 @@
             </div>
             <div class="tabs-panel full-height" id="panel4v">
                 <div class="row">
+                    <div class="large-6 small-12 columns">
+                        <label>Sort By:</label>
+                        <select>
+                            <option value="Completion date">Date</option>
+                            <option value="price">Price</option>
+                            <option value="duration">Duration</option>
+                        </select>
+                    </div>
+                    <div class="large-6 small-12 columns">
+                        <label>Search:</label>
+                        <input type="text" placeholder="Live Search"/>
+                    </div>
+                    <div class="large-12 small-12 columns"><p>Select from the following list to further interact:<p></div>
+                </div>
+                <div class="row">
                     <div class="large-12 medium-12 columns">
                         <img class="thumbnail" src="Images/tempContractorImage.png" alt="Photo of Contractor." style="width: 100px;height: 100px;">
                         <input type="radio" name="contractor" value="contractor1" id="contractor1"><label for="contractor1">Paint Co</label><label for="contractor1">R4500</label><label for="contractor1">Commencement date: 25/04/16</label><hr>
@@ -114,6 +137,21 @@
             <div class="tabs-panel full-height" id="panel5v" style="max-height: 800px;overflow-y: scroll">
                 <div class="row">
                     <div class="large-12 medium-12 columns">
+                        <div class="row">
+                            <div class="large-6 small-12 columns">
+                                <label>Sort By:</label>
+                                <select>
+                                    <option value="Completion date">Date</option>
+                                    <option value="price">Price</option>
+                                    <option value="duration">Duration</option>
+                                </select>
+                            </div>
+                            <div class="large-6 small-12 columns">
+                                <label>Search:</label>
+                                <input type="text" placeholder="Live Search"/>
+                            </div>
+                            <div class="large-12 small-12 columns"><p>Select from the following list to further interact:<p></div>
+                        </div>
                         <img class="thumbnail" src="Images/tempContractorImage.png" alt="Photo of Contractor." style="width: 100px;height: 100px;">
                         <input type="radio" name="manageJob" value="contractor1" id="contractor1"><label for="contractor1">Job 1</label><label for="contractor1">Paint Co</label><label for="contractor1">R4500</label><label for="contractor1">Completion date: 01/01/16</label><label for="contractor1">Job rating: like</label><label for="contractor1">Contractor rating: like</label><hr>
                         <img class="thumbnail" src="Images/tempUserImage.png" alt="Photo of Contractor." style="width: 100px;height: 100px;">
@@ -122,6 +160,7 @@
                         <input type="radio" name="manageJob" value="contractor3" id="contractor3"><label for="contractor3">Job 1</label><label for="contractor3">Paver Co</label><label for="contractor3">R4500</label><label for="contractor3">Completion date: 20/02/16</label><label for="contractor3">Job rating: like</label><label for="contractor3">Contractor rating: no rating provided</label><hr>
                         <img class="thumbnail" src="Images/tempUserImage.png" alt="Photo of Contractor." style="width: 100px;height: 100px;">
                         <input type="radio" name="manageJob" value="worker4" id="worker4"><label for="worker4">Job 1</label><label for="worker4">Dill Harper</label><label for="worker4">R2500</label><label for="worker4">Completion date: 15/03/16</label><label for="worker4">Job rating: like</label><label for="worker4">Worker rating: like</label>
+                        <img class="thumbnail" src="Images/tempUserImage.png" alt="Photo of Contractor." style="width: 100px;height: 100px;">
                         <input type="radio" name="manageJob" value="worker5" id="worker5"><label for="worker5">Job 2</label><label for="worker5">Dill Harper</label><label for="worker5">R1500</label><label for="worker5">Completion date: 25/03/16</label><label for="worker4">Job rating: like</label><label for="worker5">Worker rating: like</label>
                     </div>
                 </div>
